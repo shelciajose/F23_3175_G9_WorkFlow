f483f592236730401bd665eb82fe852fae644c08
Takumi08 <yonemurat@student.douglascollege.ca>
2023-11-12 12:35:48 -0800

    Add navigation bar

---
+++ app/src/main/java/com/example/workflow/ConstList.java
@@ -0,0 +1,34 @@
+package com.example.workflow;
+
+public class ConstList {
+    protected final String KEY_ID = "id";
+    protected final String KEY_USER = "user";
+    protected final String KEY_RECEIVER_USER_ID = "receiverUserId";
+    protected final String KEY_CONNECTED_USER = "connectedUser";
+    protected final String KEY_USER_NAME = "userName";
+    protected final String KEY_USER_ID = "userId";
+    protected final String KEY_IMAGE = "image";
+    protected final String KEY_IMAGE_CHAT = "imageChat";
+    protected final String KEY_ONLINE_STATUS = "onlineStatus";
+    protected final String KEY_PROCESSING = "processing";
+    protected final String KEY_PROCESSING_CHAT = "processingChat";
+    protected final String KEY_ONLINE = "online";
+    protected final String KEY_OFFLINE = "offline";
+    protected final String KEY_CHAT = "chat";
+    protected final String KEY_CHAT_LIST = "chatList";
+    protected final String KEY_DATE_FORMAT_CHAT = "dd/MM/yyyy hh:mm aa";
+    protected final String KEY_SENDER = "sender";
+    protected final String KEY_RECEIVER = "receiver";
+    protected final String KEY_MESSAGE = "message";
+    protected final String KEY_TIME_STAMP = "timeStamp";
+    protected final String KEY_DILIHAT = "dilihat";
+
+    protected final int KEY_MESSAGE_TYPE_LEFT = 0;
+    protected final int KEY_MESSAGE_TYPE_RIGHT = 1;
+    protected final String KEY_DELETE = "delete";
+    protected final String KEY_CANCEL = "cancel";
+
+    protected final String KEY_EMAIL = "email";
+
+
+}
---
+++ app/src/main/java/com/example/workflow/NavigationActivity.java
@@ -0,0 +1,71 @@
+package com.example.workflow;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class NavigationActivity extends AppCompatActivity {
+    ActionBar actionBar;
+    BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setTitle("Check in / Check out");
+        }
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setOnItemSelectedListener(selectedListener);
+
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, homeFragment,"");
+        fragmentTransaction.commit();
+    }
+
+    private final NavigationBarView.OnItemSelectedListener selectedListener = new NavigationBarView.OnItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if(item.getItemId() == R.id.menu_home) {
+                actionBar.setTitle("Home");
+
+                return true;
+            } else if(item.getItemId() == R.id.menu_profile) {
+                actionBar.setTitle("Profile");
+
+                return true;
+            } else if(item.getItemId() == R.id.menu_calendar) {
+                actionBar.setTitle("Calendar");
+
+                return true;
+            } else if(item.getItemId() == R.id.menu_leaveRequest) {
+                actionBar.setTitle("Leave Request");
+
+                return true;
+            } else if(item.getItemId() == R.id.menu_chat) {
+                actionBar.setTitle("Chat");
+
+                return true;
+
+            } else {
+                return false;
+            }
+//            else if(item.getItemId() == R.id.menu_singout) {
+//                actionBar.setTitle("Sign out");
+//                return true;
+//            }
+
+        }
+    };
+}
